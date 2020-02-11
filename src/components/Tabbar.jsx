99ed083017fa7d230b4ad1815e3d44fc58d540ac
Ayko De Smedt <ayko.de.smedt@student.howest.be>
2020-02-11 16:41:37 +0100

    tabBar css done

--- src/components/Tabbar.jsx
+++ src/components/Tabbar.jsx
@@ -1,32 +1,32 @@
 import React from "react";
-import { NavLink, useHistory } from "react-router-dom";
+import { Link, NavLink, useHistory } from "react-router-dom";
+import styles from "./Tabbar.module.css";
 
 import NavSVG from "./NavSVG";
 
 const Tabbar = () => {
   let history = useHistory();
   console.log(history.location.pathname);
-
     return (
-        <nav>
-          <ul>
+        <nav className="nav">
+          <ul className={styles.navUl}>
             <li>
-              <NavLink to="/">
+              <Link to="/" className={styles.navLi}>
                 <NavSVG icon="home" state={(history.location.pathname === "/") ? "active" : "inactive"} />
-                <span className="detail">Home</span>
-              </NavLink>
+                <span className={(history.location.pathname === "/") ? "detail accent" : "detail"}>Home</span>
+              </Link>
             </li>
             <li>
-              <NavLink to="/challenges">
+              <Link to="/challenges" className={styles.navLi}>
                 <NavSVG icon="challenges" state={(history.location.pathname === "/challenges") ? "active" : "inactive"}/>
-                <span className="detail">Challenges</span>
-              </NavLink>
+                <span className={(history.location.pathname === "/challenges") ? "detail accent" : "detail"}>Challenges</span>
+              </Link>
             </li>
             <li>
-              <NavLink to="/profile">
+              <Link to="/profile" className={styles.navLi}>
                 <NavSVG icon="challenges" state={(history.location.pathname === "/profile") ? "active" : "inactive"} />
-                <span className="detail">Profile</span>
-              </NavLink>
+                <span className={(history.location.pathname === "/profile") ? "detail accent" : "detail"}>Profile</span>
+              </Link>
             </li>
           </ul>
         </nav>
