4ff062dc886f0126584b9a52b7790e1608ec1264
Ayko De Smedt <ayko.de.smedt@student.howest.be>
2020-02-13 18:36:10 +0100

    room css done

--- src/containers/Room.jsx
+++ src/containers/Room.jsx
@@ -5,6 +5,11 @@ import { useEffect } from "react";
 import { inject, observer } from "mobx-react";
 import { useState } from "react";
 
+import TopBar from "./../components/TopBar.jsx";
+import styles from "./Room.module.css";
+import stylesUI from "../styles/ui.module.css";
+import stylesTypo from "../styles/typo.module.css";
+
 const Room = ({databaseStore, dataStore}) => {
     let {id} = useParams();
 
@@ -83,81 +88,85 @@ const Room = ({databaseStore, dataStore}) => {
 
   return (
     <>
-        <h1>Room {id}</h1>
-        {room.chat === true ? (
-            <>
-                <button onClick={() => handleChat()}>chat</button>
-                <Link to={"../" + url}>
-                    <button onClick={() => {handleAnswerQuestions()}}>Answer more questions</button>
-                </Link>
-                <button onClick={() => handleRemoveMatch()}>Remove match</button>
-            </>
-        ) : (
-            <>
-                {room.chat === localStorage.uid ? (
-                    <>
-                        <p>chat requested</p>
-                        <Link to={"../" + url}>
-                            <button onClick={() => {handleAnswerQuestions()}}>Answer more questions</button>
-                        </Link>
-                        <button onClick={() => handleRemoveMatch()}>Remove match</button>
-                    
-                        {room && user !== "" ? (
-                            <section>
-                                <h2>{room.prevGrens}</h2>
-                                <p>Your match answered:</p>
-                                <article>
-                                    <p>{room.antwoorden[user].vraag1.vraag}</p>
-                                    <p>{room.antwoorden[user].vraag1.antwoord}</p>
-                                </article>
-                                <article>
-                                    <p>{room.antwoorden[user].vraag2.vraag}</p>
-                                    <p>{room.antwoorden[user].vraag2.antwoord}</p>
-                                </article>
-                                <article>
-                                    <p>{room.antwoorden[user].vraag3.vraag}</p>
-                                    <p>{room.antwoorden[user].vraag3.antwoord}</p>
-                                </article>
-                            </section>
-                        ) : (
-                            <p>Aan het laden...</p>
-                        )}
-                    </>
-                ) : (
-                    <>
-                    <button onClick={() => handleChatRequest()}>Request to chat</button>
+        <TopBar title="empty" />
+        {/* <h1 className={stylesUI.visibilyHidden}>Room {id}</h1> */}
+        <div className={stylesUI.contentContainer}>
+            {room.chat === true ? (
+                <>
+                    <button onClick={() => handleChat()}>chat</button>
                     <Link to={"../" + url}>
-                            <button onClick={() => {handleAnswerQuestions()}}>Answer more questions</button>
-                        </Link>
+                        <button onClick={() => {handleAnswerQuestions()}}>Answer more questions</button>
+                    </Link>
                     <button onClick={() => handleRemoveMatch()}>Remove match</button>
-                    
-                        {room && user !== "" ? (
-                            <section>
-                                <h2>{room.prevGrens}</h2>
-                                <p>Your match answered:</p>
-                                <article>
-                                    <p>{room.antwoorden[user].vraag1.vraag}</p>
-                                    <p>{room.antwoorden[user].vraag1.antwoord}</p>
-                                </article>
-                                <article>
-                                    <p>{room.antwoorden[user].vraag2.vraag}</p>
-                                    <p>{room.antwoorden[user].vraag2.antwoord}</p>
-                                </article>
-                                <article>
-                                    <p>{room.antwoorden[user].vraag3.vraag}</p>
-                                    <p>{room.antwoorden[user].vraag3.antwoord}</p>
-                                </article>
-                            </section>
-                        ) : (
-                            <p>Aan het laden...</p>
-                        )}
-                    </>
-                )}
-                
-                
-                
-            </>
-        )}
+                </>
+            ) : (
+                <>
+                    {room.chat === localStorage.uid ? (
+                        <>
+                            <p>chat requested</p>
+                            <Link to={"../" + url}>
+                                <button onClick={() => {handleAnswerQuestions()}}>Answer more questions</button>
+                            </Link>
+                            <button onClick={() => handleRemoveMatch()}>Remove match</button>
+                            {room && user !== "" ? (
+                                <section>
+                                    <p className={stylesTypo.header1}>Je match antwoordde</p>
+                                    <div className={styles.widthCard}>
+                                        <article className={styles.articleVraag}>
+                                            <p className={stylesTypo.small}>{room.antwoorden[user].vraag1.vraag}</p>
+                                            <p>{room.antwoorden[user].vraag1.antwoord}</p>
+                                        </article>
+                                        <div className={styles.longLineShort}></div>
+                                        <article className={styles.articleVraag}>
+                                            <p className={stylesTypo.small}>{room.antwoorden[user].vraag2.vraag}</p>
+                                            <p>{room.antwoorden[user].vraag2.antwoord}</p>
+                                        </article>
+                                        <div className={styles.longLineShort}></div>
+                                        <article className={styles.articleVraag}>
+                                            <p className={stylesTypo.small}>{room.antwoorden[user].vraag3.vraag}</p>
+                                            <p>{room.antwoorden[user].vraag3.antwoord}</p>
+                                        </article>
+                                    </div>
+                                </section>
+                            ) : (
+                                <p>Aan het laden...</p>
+                            )}
+                        </>
+                    ) : (
+                        <>
+                            {room && user !== "" ? (
+                                <section>
+                                    <p className={stylesTypo.header1}>Je match antwoordde</p>
+                                    <div className={styles.widthCard}>
+                                        <article className={styles.articleVraag}>
+                                            <p className={stylesTypo.small}>{room.antwoorden[user].vraag1.vraag}</p>
+                                            <p>{room.antwoorden[user].vraag1.antwoord}</p>
+                                        </article>
+                                        <div className={styles.longLineShort}></div>
+                                        <article className={styles.articleVraag}>
+                                            <p className={stylesTypo.small}>{room.antwoorden[user].vraag2.vraag}</p>
+                                            <p>{room.antwoorden[user].vraag2.antwoord}</p>
+                                        </article>
+                                        <div className={styles.longLineShort}></div>
+                                        <article className={styles.articleVraag}>
+                                            <p className={stylesTypo.small}>{room.antwoorden[user].vraag3.vraag}</p>
+                                            <p>{room.antwoorden[user].vraag3.antwoord}</p>
+                                        </article>
+                                    </div>
+                                </section>
+                            ) : (
+                                <p>Aan het laden...</p>
+                            )}
+                            <button onClick={() => handleChatRequest()} className={styles.acceptButton}>Accepteer match</button>
+                            <button onClick={() => handleRemoveMatch()} className={stylesUI.button2}>Weiger match</button>
+                            <Link to={"../" + url}>
+                                <button onClick={() => { handleAnswerQuestions() }} className={styles.questionButton}>Meer vragen</button>
+                            </Link>
+                        </>
+                    )} 
+                </>
+            )}
+        </div>
     </>
   );
 };
